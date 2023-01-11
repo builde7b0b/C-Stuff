54c70261047f1e48e7a01f1eae657b6fdf885d18
Geno Grand (ecbjr) <ggives3@gmail.com>
2023-01-10 22:54:22 -0800

    Create handle.cpp

---
+++ handle.cpp
@@ -0,0 +1,22 @@
+// A simple exception handling example.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Start\n";
+  
+  try { // start a try block
+    cout << "Inside try block\n";
+    throw 100; // throw an error
+    cout << "This will not execute";
+  }
+  catch (int i) { // catch an error
+    cout << "Caught an exception -- value is: ";
+    cout << i << "\n";
+  }
+  
+  cout << "End";
+  return 0;
+}
+
